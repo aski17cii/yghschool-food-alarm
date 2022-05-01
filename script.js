a384e848be55a923bff4a3b815f7eb9f35cc2ad7
kwonjungo <aski17cii@gmail.com>
2022-05-01 12:29:01 +0900

    급식 정보가 없을 때 반응 추가

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var year = today.getFullYear(); // 년도
 var month = (today.getMonth() + 1).toString().padStart(2,'0');  // 월
 var date = today.getDate().toString().padStart(2,'0');  // 날짜
 
-document.querySelector('#YMD').value = year + '-' + month + '-' + date
+document.querySelector('#YMD').value = `${year}-${month}-${date}`
 printFoodList()
 
 var btn = document.querySelector('#printBtn')
@@ -13,17 +13,15 @@ function printFoodList() {
 
   var replaceDate = document.querySelector('#YMD').value.replace(/-/g, '')
   
-  var apiURL= "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=f1a5ea919bda4471bc6d1517a067a92c&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531105&MLSV_YMD="+replaceDate
+  var apiURL= `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=f1a5ea919bda4471bc6d1517a067a92c&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531105&MLSV_YMD=${replaceDate}`
   
   fetch(apiURL)
   .then((response) => response.json())
   .then((data) => {
     var foodList = data.mealServiceDietInfo[1].row[0].DDISH_NM
-    document.querySelector('#foodList').innerHTML = foodList});
+    document.querySelector('#foodList').innerHTML = foodList
+  })
+  .catch((error) => {
+    document.querySelector('#foodList').innerHTML = "급식 정보가 없습니다."
+  })
 }
-
-
-    
-
-
-
